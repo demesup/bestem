f47a620f993009fe3e738ff2601b18112061a1c9
Sofiia Huzhan <100019943+demesup@users.noreply.github.com>
2025-12-06 04:55:01 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,9 +18,9 @@ async def on_ready():
     scheduler.start()
 
     # Schedule starting 16:40 every minute for testing
-    scheduler.add_job(send_checkin, "cron", hour=4, minute="52-52")  # 16:40, 16:41, 16:42
-    scheduler.add_job(send_opening, "cron", hour=4, minute="53-53")  # 16:43, 16:44, 16:45
-    scheduler.add_job(send_giveaway, "cron", hour=4, minute="55-55")  # 16:46, 16:47, 16:48
+    scheduler.add_job(send_checkin, "cron", hour=4, minute="56-56")  # 16:40, 16:41, 16:42
+    scheduler.add_job(send_opening, "cron", hour=4, minute="58-58")  # 16:43, 16:44, 16:45
+    scheduler.add_job(send_giveaway, "cron", hour=4, minute="57-57")  # 16:46, 16:47, 16:48
 
 async def send_checkin():
     channel = bot.get_channel(CHANNEL_ID)
